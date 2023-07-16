9f2c9b145ac8650a1c3c267ac1bbd7b30c8b512a
Yulian <yulianst7@gmail.com>
2023-07-16 19:42:41 +0300

    Refactor files
    Refactor some View files with routes

--- NodeJS_Express/Strams and Utils/FirstSolution/main.js
+++ NodeJS_Express/Strams and Utils/FirstSolution/main.js
@@ -1,41 +1,53 @@
 const http = require('http')
 const url = require('url')
+const pubSub = require('./pubSub')
 
 //Класа ULR ни дава директен достъп до href,protocol,username,password,host,hostname,search,SearchParams[Същотото като querryString],hash
 // const url = new URL('/catalog?page=4#pricing', 'http://localhost:5000')
 const fs = require('fs')
 const queryString = require('querystring')
 const port = 5000;
+const names = []
 
-console.log(url);
+//pub/sub example
+const onHomeRequest = (name) => {
+    if (names.includes(name)) {
+        console.log(`Name ${name} hello again `);
+    } else {
+        console.log(`We have new cat - ${name}`);
+        names.push(name)
+    }
+    console.log(`We have new Cat - ${name}`);
+}
+
 function requestHavndler(req, res) {
     let reqUrl = url.parse(req.url)
     let params = queryString.parse(reqUrl.query)
 
-    // console.log(params);
-    // console.log(reqUrl);
 
     switch (reqUrl.pathname) {
-        case '/home':
+        case '/cats':
             res.writeHead(200, {
                 'Content-Type': 'text/html'
             })
-            fs.readFile('./Views/home.html', (err, data) => {
+            fs.readFile('./View/cats.html', (err, data) => {
                 if (err) {
-                    console.log('Error');
+                    console.log('Error', err);
                     return;
                 }
                 // console.log(data);
                 res.write(data)
                 res.end()
             })
+            //pub/sub example
+            onHomeRequest(params.name)
             break;
 
-        case '/second':
+        case '/dogs':
             res.writeHead(200, {
                 'Content-Type': 'text/plain'
             })
-            res.write('Second Page')
+            res.write('dogs Page')
             res.end()
             break;
 
