179b8fd3aa11cd1732b933a21fc47d2a30c0cbff
Yulian <yulianst7@gmail.com>
2023-07-18 14:57:12 +0300

    Install express

--- NodeJS_Express/Strams and Utils/SecondSolution/Serverdemo/index.js
+++ NodeJS_Express/Strams and Utils/SecondSolution/Serverdemo/index.js
@@ -7,9 +7,11 @@ http.createServer((req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`)
 
     if (url.pathname == '/') {
-        fs.createReadStream('./static/index.html').pipe(res)
-    } else if (url.pathname == '/catalogue.html') {
-        fs.createReadStream('./static/catalogue.html').pipe(res)
+        res.writeHead(301, {
+            Location: '/index.html'
+        })
+    } else if (url.pathname.slice(-5) == '.html') {
+        fs.createReadStream(`./static/${url.pathname}`).pipe(res)
     }
 
 }).listen(port || 3000, () => console.log(`Server started on ${port}`));
