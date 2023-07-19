c0387c94bcc802de2d72327a84077bce0b13be8f
Yulian <yulianst7@gmail.com>
2023-07-19 16:04:38 +0300

    Add view engine

--- NodeJS_Express/ExpressTemplatingWorshop/FirstSolution/index.js
+++ NodeJS_Express/ExpressTemplatingWorshop/FirstSolution/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const config = require('./config/config')
-
-const port = config.PORT
-
-
 const app = express()
 
-app.listen(port, () => console.log(`Server started at port ${port}`));
+const port = config.PORT || 5000
+//-----------------------------------------------
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+//-----------MIDDLEWARE--------------------------
+app.engine('hbs', engine({ extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
+//-----------------------------------------------
+app.get('/', (req, res) => {
+  // res.send('its working')
+  res.render('home', { layout: false })
+})
+//-----------ACTIONS-----------------------------
+
+app.listen(port, () => console.log(`Server started at port ${port}`)); 
