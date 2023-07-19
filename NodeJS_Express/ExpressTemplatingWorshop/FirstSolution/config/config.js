836596a04d3a9759cb908a33f8521b63063c1341
Yulian <yulianst7@gmail.com>
2023-07-19 11:10:33 +0300

    Creating project
    Creating config file
    Adding dev and prod configurations in package.json

---
+++ NodeJS_Express/ExpressTemplatingWorshop/FirstSolution/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    development: {
+        PORT: 5000
+    },
+    production: {
+        PORT: 80
+    }
+}
+module.exports = config[process.env.NODE_ENV]
---
+++ NodeJS_Express/ExpressTemplatingWorshop/FirstSolution/index.js
@@ -0,0 +1,5 @@
+const express = require('express')
+
+const app = express()
+
+app.listen(port, () => console.log(`Server started at port ${port}`));
