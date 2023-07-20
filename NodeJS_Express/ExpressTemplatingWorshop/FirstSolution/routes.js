222e1671c81e559039b771c1e67b9f99eaf56550
Yulian <yulianst7@gmail.com>
2023-07-20 15:35:01 +0300

    Route conflict
    /* route is conflicting with the rest of the routes
    therefore cant find the path for the public page

--- NodeJS_Express/ExpressTemplatingWorshop/FirstSolution/routes.js
+++ NodeJS_Express/ExpressTemplatingWorshop/FirstSolution/routes.js
@@ -4,13 +4,16 @@ const { Router } = require('express')
 const router = Router()
 
 // router.route()
-
-
 router.use('/', productRoutes)
+
+// Specific route handler for /about
 router.use('/about', aboutRoute)
 
-// router.use('/something', (req, res) => {
-//     res.render('404')
-// })
+// Catch-all 404 route, place it after other routes
+router.use('*', (req, res) => {
+    console.log('inside 404');
+    // res.render('404')
+})
+
 
 module.exports = router
